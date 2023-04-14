26bb97d0556030fc6561919a51a6bc9605cd0f87
sergio <apontelamussergioandres@gmail.com>
2023-04-14 09:11:21 -0500

    house

--- components/vistasecond.vue
+++ components/vistasecond.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="contenedor_principal">
-        <div><h2>{{ nombre }}</h2></div>
-        <div>
-            <div><img :src="url" alt=""></div>
+        <h2>{{ nombre }}</h2>
+        <div class="general">
+            <div class="imagen_general"><img :src="url" alt=""></div>
             <div class="cuadro_habilidades">
                 <div class="squirils">
-                    <h2>Status</h2>
+                    <h3 class="status">Status</h3>
                     <p v-for="i in response.stats"  :key="i.stat.name"  >{{ i.stat.name }}</p>
                 </div>
                 <div class="habilities">
-                    <h2>abilities</h2>
+                    <h3>abilities</h3>
                     <p v-for=" i in response.abilities" :key="i.ability.name">{{ i.ability.name }}</p>
                 </div>
-                <div class="imagenes_segundarias">
-                    <img :src="imagenes.back_default" alt="">
-                    <img :src="imagenes.back_shiny" alt="">
-                    <img :src="imagenes.front_shiny" alt="">
-                    <img :src="imagenes.front_default" alt="">
-                </div>
+            </div>
+            <div class="imagenes_segundarias">
+                <img :src="imagenes.back_default" alt="">
+                <img :src="imagenes.back_shiny" alt="">
+                <img :src="imagenes.front_shiny" alt="">
+                <img :src="imagenes.front_default" alt="">
             </div>
         </div>
     </div>
@@ -55,11 +55,127 @@ export default{
 </script>
 
 <style>
-img{
-    height: 200px;
-    width: 150px;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}  
+
+.contenedor_principal{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-image: url(../img/X8ms9l2.webp);
+    background-repeat: no-repeat;
+    background-size: cover;
 }
-p{
+
+.contenedor_principal .general{
+    display: grid;
+    height: 90%;
+    width: 100%;
+    grid-template-areas: 
+    'parte1  parte2 '
+    'parte3 parte3 '
+    ;
+}
+
+.imagen_general{
+    grid-area: parte1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 7px;
+}
+
+.imagen_general img{
+    height: 70%;
+    width: 70%;
+}
+
+
+.cuadro_habilidades{
+    grid-area: parte2;
+    display: grid;
+    grid-template-areas: 
+    'parte1 parte2'
+    ;
+    gap: 5px;
+}
+
+.squirils{
+    grid-area: parte1;
+    border: 5px solid;
+    width: 400px;
+    border-radius: 7px;
+    background-color: red;
+
+}
+.habilities{
+    grid-area: parte2;
+    border: 5px solid black;
+    background-color: rgb(241, 233, 233);
+    width: 400px;
+    border-radius: 7px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
+.habilities p{
     color: black;
+}
+
+
+.imagenes_segundarias{
+    grid-area: parte3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 40px;
+}
+
+.imagenes_segundarias img{
+    height: auto;
+    width: 150px;
+    border: 3px solid;
+    background-color: rgba(255, 255, 255, 0.623);
+    border-radius: 7px;
+}
+
+h2{
+    width: 90%;
+    height: 10%;
+    font-size: 45px;
+    background-color: white;
+    border: 1px solid;
+    border-radius: 7px ;
+    text-align: center;
+    margin: 20px 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h3{
+    width: 90%;
+    height: 10%;
+    font-size: 35px;
+    background-color: white;
+    border: 1px solid;
+    border-radius: 7px ;
+    text-align: center;
+    margin: 20px 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+p{
+    font-size: 30px;
+    color: white;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,152 @@
 <template>
-  <Tutorial/>
+    <div class="principal w-full">
+        <h1>Poke Dex</h1>
+        <main class="contenido">
+            <form action="" @submit.prevent="buscar" >
+                <input id="busqeda" type="text" placeholder="Buscar Pokemon"  v-model="search">
+            </form>
+            <div class="main_tarjeta_contenedor">
+                <div  v-for="i in response" :key="i.name">
+                    <card :nombre="i.name" :url="i.sprites.other.dream_world.front_default"/>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
-
 <script>
+
+
 export default {
-  name: 'IndexPage'
+    name: 'PokePage',
+    data(){
+        return{
+            response:[
+            ],
+            search:''
+        }
+    },
+    mounted(){
+        this.getinfo()
+        this.buscar()
+    },
+    methods:{
+        async getinfo(){
+            for(let j=1;j<20;j++){
+            const {data}= await this.$axios.get(`pokemon/${j}`)
+            this.response.push(data)
+            console.log(this.response)
+        };
+        // buscar(){
+        //     for(let i=0;i<this.response.length;i++){
+        //         if(this.data[i].name==this.search){
+        //             this.$router.push(`/${this.search}`)
+        //         }
+        //         console.log(this.search)
+        //     }
+        // }
+        },
+        buscar(){
+            for(let i=0;i<this.response.length;i++){
+                if(this.data[i].name==this.search){
+                    this.$router.push(`/${this.search}`)
+                }
+                console.log(this.search)
+            }
+        }
+    }
 }
 </script>
 
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+} 
+
+
+.principal{
+    width: 100vw;
+    height: auto;
+    border: 1px solid;
+    background-color: aqua;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.main_tarjeta_contenedor{
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-wrap: wrap; 
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+}
+
+
+h1{
+    width: 90%;
+    height: 10%;
+    font-size: 50px;
+    background-color: white;
+    border: 1px solid;
+    border-radius: 7px ;
+    text-align: center;
+    margin: 20px 10px;
+}
+main{
+    width: 100%;
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 20px 10px;
+}
+input{
+    width: 200px;
+    height: 30px;
+    border: 1px solid black;
+    border-radius: 7px;
+    margin-left: 50px;
+}
+/* .tarjetas{
+    height: 300px;
+    width: 250px;
+    border: 1px solid;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+}
+.fondo{
+    height: 95%;
+    width: 95%;
+    background-color: red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    border: 1px solid;
+}
+
+img{
+    height: 90%;
+    width: 90%;
+}
+p{
+    color: white;
+} */
+P{
+    color: white;
+}
+
+
+</style>
